b1913d0fef097aa166db9da7178824d5735b12e1
wwwxsv19 <wwwxsv19@gmail.com>
2023-10-21 23:32:09 +0900

    ~1021

---
+++ 구룸/6단원 자료구조4_2차원배열 구조체 포인터/6_6 원점가까운좌표.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct point{
+	int x, y;
+} POINT;
+
+void inputPoint( POINT *p ){
+	for(int i=0; i<5; i++){
+		scanf("%d %d", &p[i].x, &p[i].y);
+	}
+}
+	
+void outputPoint( POINT p ){
+	printf("(%4d, %4d)\n", p.x, p.y);
+}
+
+POINT distance(POINT *p) {
+	POINT c = p[0];
+	int n = abs(p[0].x) + abs(p[0].y);
+
+	for (int i = 1; i < 5; i++) {
+		int m = abs(p[i].x) + abs(p[i].y);
+		if (m < n) {
+			c = p[i];
+			n = m;
+		}
+		else if (n == m) {
+			int j = p[i].x + p[i].y;
+			int k = c.x + c.y;
+			if (j < k) c = p[i];
+		}
+	}
+	
+	return c;
+}
+
+int main() {
+	POINT p[5];
+	POINT origin;
+	
+	inputPoint(p);
+	
+	origin = distance(p);
+	
+	printf("원점에서 가장 가까운 좌표 : ");
+	outputPoint(origin);
+	
+	return 0;
+}
