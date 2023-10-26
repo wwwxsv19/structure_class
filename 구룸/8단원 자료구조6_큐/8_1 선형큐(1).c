06ea5c5e41ee1af9ea38bc5e87ea6a56e5909ec8
wwwxsv19 <wwwxsv19@gmail.com>
2023-10-26 22:09:58 +0900

    ~1026

---
+++ 구룸/8단원 자료구조6_큐/8_1 선형큐(1).c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_Q_SIZE 5
+
+typedef int element;
+
+typedef struct{
+	int front;
+	int rear;
+	element data[MAX_Q_SIZE];
+} Queue;
+
+void error(char* message){
+	printf("%s\n", message);
+	exit(0); // 메인함수 종료
+}
+
+void initQueue(Queue* q){
+	q->front = q->rear = -1;
+}
+int isFull(Queue* q){
+	return q->rear == MAX_Q_SIZE-1;
+}
+int isEmpty(Queue* q){
+	return q->rear == q->front;
+}
+
+void enQueue(Queue* q, element item){
+	if(isFull(q)){
+		error("Queue is full\n");
+	}
+	else{
+		q->data[++q->rear] = item;
+	}
+}
+
+element deQueue(Queue* q){
+	int temp;
+	if(isEmpty(q)){
+		error("Queue is empty\n");
+	}
+	else{
+		return q->data[q->front++];
+	}
+}
+	
+void queuePrint(Queue* q){
+	int i;
+	for(i=0; i<MAX_Q_SIZE; i++){
+		if (i <= q->front || i > q->rear){
+			printf(" | ");
+		}
+		else{
+			printf("%d | ", q->data[i]);
+		}
+	}
+	printf("\n");
+}
+
+int main() {
+	element item = 0;
+	Queue q;
+	
+	initQueue(&q);
+	
+	enQueue(&q,10); queuePrint(&q);
+	enQueue(&q,20); queuePrint(&q);
+	enQueue(&q,30); queuePrint(&q);
+	
+	item = deQueue(&q); queuePrint(&q);
+	item = deQueue(&q); queuePrint(&q);
+	
+	return 0;
+}
---
+++ 구룸/8단원 자료구조6_큐/8_2 선형큐(2).c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element ;
+
+typedef struct {
+	element* data;
+	int front;
+	int rear;
+	int size;
+} Queue;
+
+Queue* initQueue(int n){
+	Queue *temp = (Queue*)malloc(sizeof(Queue)); 
+	temp->size = n; // n을 크기로 하는 큐
+	temp->rear = temp->front = -1;
+	temp->data = (int*)malloc(sizeof(int) * n); // 크기 n을 가지는 동적 배열 
+	
+	return temp;
+}
+int isFull( Queue *q ){
+	return q->rear == q->size-1;
+}
+int isEmpty( Queue *q ){
+	return q->rear == q->front;
+}
+
+void enQueue( Queue *q, element data ){
+	if(isFull(q)){
+		printf("Queue is full\n");
+		return ;
+	}
+	q->data[++q->rear] = data;
+}
+element deQueue( Queue *q ){
+	if(isEmpty(q)){
+		printf("Queue is empty\n");\
+		return 0;
+	}
+	return q->data[q->front++];
+}
+void printAll( Queue *q ){
+	int i;
+	for(i=0; i<q->size; i++){
+		if (i <= q->front || i > q->rear){
+			printf(" | ");
+		}
+		else{
+			printf("%d | ", q->data[i]);
+		}
+	}
+	printf("\n");
+}
+
+void destroyQueue( Queue * q ){
+	free(q->data);
+	free(q);
+}
+
+int main() {
+	Queue *q;
+	element data;
+
+	q = initQueue(5); 
+	
+	enQueue(q, 10); printAll(q);
+	enQueue(q, 20); printAll(q);
+	enQueue(q, 30); printAll(q);
+	enQueue(q, 40); printAll(q);
+	enQueue(q, 50); printAll(q);
+	
+	data = deQueue(q); printAll(q);
+	
+	enQueue(q, data); printAll(q);
+	
+	destroyQueue( q );
+	
+	return 0;
+}
