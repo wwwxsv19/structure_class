9be93f68d028afcb9a5080e4d08800c29f23bb97
wwwxsv19 <wwwxsv19@gmail.com>
2023-10-29 23:21:41 +0900

    ~1029

---
+++ 구룸/9단원 자료구조7_연결리스트/9_1 연결리스트 구현.c
@@ -0,0 +1,37 @@
+// 동적으로 구조체 생성하기
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct NODE{
+	element data;
+	struct Node* link;
+} Node;
+
+int main(){
+	Node *n1, *n2 ,*n3, *head;
+
+	n1 = (Node*)malloc(sizeof(Node));
+	n2 = (Node*)malloc(sizeof(Node));
+	n3 = (Node*)malloc(sizeof(Node));
+	
+	n1->data = 10;
+	n1->link = n2;
+	
+	n2->data = 20;
+	n2->link = n3;
+	
+	n3->data = 30;
+	n3->link = NULL;
+	
+	head = n1;
+	
+	while(head){
+		printf("%d->", head->data);
+		head = head->link;
+	}
+
+	return 0;
+}
