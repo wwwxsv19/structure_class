c4dfeacda9dc75212ed80ce3b548c6b1a9ccd4fc
wwwxsv19 <wwwxsv19@gmail.com>
2023-11-01 00:14:21 +0900

    ~1101

---
+++ 구룸/9단원 자료구조7_연결리스트/9_4 단연리_삽입탐색.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node{
+    char data[4];
+    struct Node* next;
+} Node;
+
+void deleteAll(Node* head){
+    Node* removed;
+    while(head != NULL){
+        removed = head;
+        head = head->next;
+        free(removed);
+        removed = removed->next = NULL;
+    }
+}
+
+void printList(Node* head){
+    Node* p = head;
+    printf("L = (");
+    while(p != NULL){
+        printf("%s", p->data);
+        p = p->next;
+        if(p != NULL) printf("->");
+    }
+    printf(") \n");
+}
+
+Node* insertFirst(Node* head, char* x){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    strcpy(newNode->data, x);
+    newNode->next = head;
+    return newNode;
+}
+
+Node* insertMiddle(Node* head, Node* pre, char* x){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    strcpy(newNode->data, x);
+
+    if(head == NULL || pre == NULL){
+        newNode->next = head;
+        head = newNode;
+    }
+    else{
+        newNode->next = pre->next;
+        pre->next = newNode;
+    }
+    return head;
+}
+
+Node* insertLast(Node* head, char* x){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    Node* temp;
+
+    strcpy(newNode->data, x);
+    newNode->next = NULL;
+    if(head == NULL) return newNode;
+
+    temp = head;
+    while(temp->next != NULL){
+        temp = temp->next; 
+    }
+    temp->next = newNode;
+    return head;
+}
+
+Node* searchNode(Node* head, char* x){
+    Node* temp = head;
+
+    while(temp != NULL){
+        if(strcmp(temp->data, x) == 0) return temp;
+        else temp = temp->next;
+    }
+    return temp;
+}
+
+int main(){
+	Node* head;
+	Node* p;
+
+	printf("(1) 공백리스트 생성하기! \n");
+	head = NULL;
+	printList(head);
+	
+	printf("(2) 리스트 처음에 [수]노드 삽입하기! \n");
+	head =insertFirst( head, "수");
+	printList(head);
+	
+	printf("(3) 리스트 마지막에 [금]노드 삽입하기! \n");
+	head =insertLast( head, "금");
+	printList(head);
+	
+	printf("(4) 리스트 첫 번째에 [월]노드 삽입하기! \n");
+	head =insertFirst( head, "월");
+	printList(head);
+	
+	printf("(5) 리스트에서 [수] 노드를 찾아 그 뒤에 [목] 삽입하기! \n");
+	p = searchNode(head, "수");
+	if(p == NULL) printf("찾는 데이터가 없습니다.\n");
+	else head = insertMiddle( head, p, "목");
+	printList( head );
+	
+	printf("(6) 리스트 공간을 해제하여 공백 리스트로 만들기! \n");
+	deleteAll(head);
+	printList(head);
+
+	return 0;
+}
---
+++ 구룸/9단원 자료구조7_연결리스트/9_5 단연리_탐색.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node{
+    char data[100];
+    struct Node* next;
+} Node;
+
+Node* insertFirst(Node* head, char* data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    strcpy(newNode->data, data);
+    newNode->next = head;
+    return newNode;
+}
+
+Node* insert(Node* head, Node* pre, char* data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    strcpy(newNode->data, data);
+    newNode->next = pre->next;
+    pre->next = newNode;
+    return head;
+}
+
+Node* deleteFirst(Node* head){
+    if(head == NULL) return NULL;
+    Node* removed = head;
+    Node* newHead = head->next;
+    free(removed);
+    return newHead;
+}
+
+Node* delete(Node* head, Node* pre){
+    Node* removed = pre->next;
+    pre->next = removed->next;
+    free(removed);
+    return head;
+}
+
+void printList(Node* head){
+    while(head != NULL){
+        printf("%s->", head->data);
+        head = head->next;;
+    }
+    printf("NULL\n");
+}
+
+int main(){
+    Node* head = NULL;
+    Node* p;
+
+    char str[3][10] = {"melon", "kiwi", "apple"};
+    char find[10];
+
+    head = insertFirst(head, str[0]);
+    printList(head);
+
+    head = insertFirst(head, str[1]);
+    printList(head);
+
+    head = insertFirst(head, str[2]);
+    printList(head);
+
+    printf("찾을 데이터를 입력하세요\n");
+    scanf("%s", find);
+    for(p=head; p!=NULL; p=p->next){
+        if(strcmp(p->data, find) == 0){
+            printf("%s을 찾았습니다.", find);
+        }
+    }
+
+    return 0;
+}
