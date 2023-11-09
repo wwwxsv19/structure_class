772de608bee4f945b7a93ac049dbe73af95da883
wwwxsv19 <wwwxsv19@gmail.com>
2023-11-09 16:28:39 +0900

    ~1109

---
+++ 구룸/11단원 자료구조9_트리/11_1 트리_배열구현.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void preorder(char t[], int i, int size){
+	if(t[i] == NULL || i > size) return ;
+	printf("%c ", t[i]);
+	preorder(t, i*2, size);
+	preorder(t, (i*2)+1, size);
+}
+
+void inorder(char t[], int i, int size){
+	if(t[i] == NULL || i > size) return ;
+	inorder(t, i*2, size);
+	printf("%c ", t[i]);
+	inorder(t, (i*2)+1, size);
+}
+
+void postorder(char *t, int i, int size){
+	if(t[i] == NULL || i > size) return ;
+	postorder(t, i*2, size);
+	postorder(t, (i*2)+1, size);
+	printf("%c ", t[i]);
+}
+
+int main(){
+	char tree[] = {NULL, 'A', 'B', 'C', 'D', 'E', 'F', 'G', NULL, NULL, 'H', 'I', NULL, 'J', 'K', 'L'};
+	int size = 16;
+	
+	printf("\n<<전위 탐색>>\n");
+	preorder(tree, 1, size);
+	
+	printf("\n<<중위 탐색>>\n");
+	inorder(tree, 1, size);
+	
+	printf("\n<<후위 탐색>>\n");
+    postorder(tree, 1, size);
+	
+	return 0;
+}
---
+++ 구룸/11단원 자료구조9_트리/11_2 트리_연리구현.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+typedef struct TNode{
+	char data;
+	struct TNode* left;
+	struct TNode* right;
+} TNode;
+
+TNode* makeRootNode(char data, TNode* leftNode, TNode* rightNode){
+	TNode* root = (TNode*)malloc(sizeof(TNode));
+	root->data = data;
+	root->left = leftNode;
+	root->right = rightNode;
+	return root;
+}
+
+void preorder(TNode* root){
+	TNode* x = root;
+	if(x == NULL) return ;
+	printf("%c", x->data);
+	preorder(x->left);
+	preorder(x->right);
+}
+
+void inorder(TNode* root){
+	TNode* x = root;
+	if(x == NULL) return ;
+	inorder(x->left);
+	printf("%c", x->data);
+	inorder(x->right);
+}
+
+void postorder(TNode* root){
+	TNode* x = root;
+	if(x == NULL) return ;
+	postorder(x->left);
+	postorder(x->right);
+	printf("%c", x->data);
+}
+
+int main(){
+	TNode* n7 = makeRootNode('D', NULL, NULL);
+	TNode* n6 = makeRootNode('C', NULL, NULL);
+	TNode* n5 = makeRootNode('B', NULL, NULL);
+	TNode* n4 = makeRootNode('A', NULL, NULL);
+	TNode* n3 = makeRootNode('/', n6, n7);
+	TNode* n2 = makeRootNode('*', n4, n5);
+	TNode* n1 = makeRootNode('-', n2, n3);
+
+	printf("\n preorder  : ");
+	preorder(n1);
+	
+	printf("\n inorder   : ");
+	inorder(n1);
+	
+	printf("\n postorder : ");
+	postorder(n1);
+	
+	free(n1); free(n2); free(n3);
+	free(n4); free(n5); free(n6);
+	free(n7);
+	
+	return 0;
+}
