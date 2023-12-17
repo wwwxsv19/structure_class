b4a46d236e0acb532c918f4998f2f1878cf9d40b
wwwxsv19 <wwwxsv19@gmail.com>
2023-12-17 11:53:17 +0900

    ~1217

--- 구룸/10단원 자료구조8_연결리스트2/10_5 연리_스택구현.c
+++ 구룸/10단원 자료구조8_연결리스트2/10_5 연리_스택구현.c
@@ -23,7 +23,7 @@ void push(Node* top, element data){
 	top->next = node;
 }
 
-void pop(Node* top){
+element pop(Node* top){
 	element data;
 	Node* temp;
 
@@ -33,8 +33,10 @@ void pop(Node* top){
 	} // made by wwwxsv19
 	else{
 		temp = top->next;
+		data = temp->data;
 		top->next = temp->next;
 		free(temp);
+		return data;
 	}
 }
 
